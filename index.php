ee42349583e00f594d07629a24b9d0c3008ec8a5
Yoshihide Chubach (Luna CentOS) <yoshi@chubachi.net>
2011-12-26 00:50:36 +0900

    Commited

--- index.php
+++ index.php
@@ -4,6 +4,34 @@
   <title>
     産業技術大学院大学 / AIIT: Advanced Institute of Industrial Technology
   </title>
+  <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+  <meta name="author" content="IN.FIGHT" />
+  <link rel="shortcut icon" href="static/favicon.ico" type="image/vnd.microsoft.icon" />
+
+  <!-- stylesheet -->
+  <link rel="stylesheet" type="text/css" href="static/stylesheets/main.css" />
+
+  <!-- slideshow -->
+  <link rel="stylesheet" type="text/css" href="static/slideshow/slideshow.css" media="screen" />
+  <script type="text/javascript" src="static/slideshow/mootools.js"></script>
+  <script type="text/javascript" src="static/slideshow/slideshow.js"></script>
+  <script type="text/javascript" src="static/slideshow/slideshow.kenburns.js"></script>
+  <script type="text/javascript">
+//<![CDATA[
+  window.addEvent('domready', function(){
+    var data = {
+      'DSC00449.JPG': { caption: 'AIIT' }, 
+      'DSC00539.JPG': { caption: 'AIIT' }, 
+      'DSC00448.JPG': { caption: 'AIIT' }, 
+/*      'DSC00414.JPG': { caption: 'AIIT' },  */
+      'DSC00445.JPG': { caption: 'AIIT' }, 
+      'DSC00426.JPG': { caption: 'AIIT' }, 
+      'DSC00534.JPG': { caption: 'AIIT' }, 
+    };
+    var myShow = new Slideshow.KenBurns('show', data, {captions: false, deley: 5000, duration: 8000, height: 260, hu: 'static/photos/', width: 520, href: 'http://aiit.ac.jp/', random: true });
+  });
+//]]>
+  </script>
 </head>
 
 <body id="aiit-fb-welcome-app">
@@ -24,11 +52,65 @@
   </header>
 
   <section>
+    <!-- Slideshow -->
+    <div>
+      <a href="/"><div id="show" class="slideshow"></div></a>
+    </div>
+  </section>
+
+  <section>
     <p>
       Facebook上に本学公式ページを立ち上げました。
       このページ等のFacebook上で，本学の関する皆さんが本学をきっかけにして意義ある交流を深めることを期待します。
       このページには，<em>AIIT公式Webからの記事</em>も掲載されますが，在学生・修了生らの関係者の皆さんからの<em>本学に関する投稿（記事・イベント情報等）</em>あるいは<em>本学の関係者にとって興味深い投稿</em>も歓迎します。
     </p>
+
+    <div class="fb-like" data-href="http://www.facebook.com/aiit.ac.jp" data-send="false" data-width="520" data-show-faces="true"></div>
+    <p>
+      よろしければ↑の<img src="static/images/iine.png" />あるいは<img src="static/images/like.png" />
+      を押してFacebookの本学公式ページに登録してください。ご登録いただくと以下のいいことがあります。
+    </p>
+    <ul>
+      <li> 本学公式ページのウォールに投稿された記事は，皆さんの<em>ニュースフィードに自動で表示されます</em>。ウォールには，AIIT公式Webからの記事が自動的に掲載され，また本学の関係者からの記事も投稿されます。いちいちこのページを巡回する必要はありません。</li>
+      <li> 次回から<em>デフォルト</em>で本学公式ページのウォールが表示されるように変更されます。</li>
+    </ul>
+  </section>
+
+  <section>
+    <br/>
+    <p>
+      本学関係サイト: 
+    </p>
+    <ul>
+      <li>
+	AIIT Webサイト <a href="http://aiit.ac.jp/">http://aiit.ac.jp/</a>
+      </li>
+      <li>
+	ポータルサイト <a href="http://readme.aiit.ac.jp/">http://readme.aiit.ac.jp/</a>
+	<br/>
+	授業動画コンテンツはこちらから視聴できます。ポータルサイトの利用には，在学時のID及びパスワードが必要です。不明であれば，本学サポートスタッフ（<a href="mailto:support@aiit.ac.jp">mailto:support@aiit.ac.jp</a>）まで連絡してください。
+      </li>
+      <li>
+	Twitterアカウント <a href="https://twitter.com/#!/aiit4u">@aiit4u</a>
+      </li>
+      <li>
+	InfoTalk（IT勉強会）<a href="http://www.facebook.com/infotalking">Facebookページ</a>
+	<a href="http://pk.aiit.ac.jp/">Webサイト</a>
+	<br/>
+	第3金曜日18:30から定例で開催していますので，是非ご参加ください。
+      </li>
+    </ul>
+  </section>
+  <footer>
+    <blockquote>
+      <p>
+	投稿いただいた内容（Facebook上の名前・記事・写真・コメント等）は，<em>広く公開掲載されるという意識</em>を持って投稿してください。本学は交流の場を提供しているのみであり，投稿の投稿の内容・コメント等に責任を負っていません。ただし，本学が適切では無いと判断する投稿等を<em>本学の裁量によって削除する</em>権利を留保します。
+      </p>
+      <p>
+	またFacebookの規約は以下をご参照下さい。
+      </p>
+      <a href="http://www.facebook.com/terms.php">http://www.facebook.com/terms.php</a>
+    </blockquote>
   </footer>
 </body>
 
